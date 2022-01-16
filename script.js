4a04523895775220a2e13a1f90581a909ae9fb97
Amanda Pires <amanda_oliveirapires@hotmail.com>
2022-01-16 08:48:04 -0300

    adcionar estilo para tropicos

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
+
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status(data);
+    });
+}
+
+ botao.onclick = pegarPersonagem;
